85450886acae783fd70b118cac501b42aa1b9cb8
kiamco <kiamcoo@gmail.com>
2019-09-14 12:00:47 -0700

    final

--- client/src/Components/Tests/playerChart.test.js
+++ client/src/Components/Tests/playerChart.test.js
@@ -3,16 +3,9 @@ import {
     render,
     getByText
 } from '@testing-library/react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-
-
-
+import {Tooltip} from 'recharts';
 import Chart from '../playerChart.js';
 import data from '../../../../data.js'
-
-// test('is button rendering', () => {
-//     const buttons = render(< Button />);
-// })
 
 test('is button rendering', () => {
     render(<Chart data={data} />)
